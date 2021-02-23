9fbf5034f52e9f2a79d5102f491e0c4b09745663
Guru18650 <guru18650@users.noreply.github.com>
2021-02-23 16:41:21 +0100

    bruh update

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-
 var cron = require("cron");
 const discord = require ('discord.js');
 const { Client, Message } = require('discord.js');
@@ -9,202 +8,121 @@ var d = new Date();
 const prefix = "p!";  
 //client.zadania = require ("./zadania.json");
 const fs = require("fs");
+//const plan = require('./def/plan_lekcji.json')
+
+
+var zero =  ['', '', '', '', 'ABDN / ', 'PSI'];
+var one =   ['', 'PBD', '', 'WF / ', 'TSI / ', 'PSI'];
+var two =   ['', 'PPr', 'Biologia', 'WF / ', 'TSI / Informatyka', 'Chemia'];
+var three = ['', 'PPr', 'Edukacja Regionalna', 'PBD', 'Informatyka / TSI', 'Religia'];
+var four =  ['', 'Informatyka Stosowana', 'Angielski / Niemiecki', 'Godzina Wychowawcza', ' / TSI', 'Filozofia'];
+var five =  ['', 'Niemiecki / WF', 'Informatyka Stosowana', 'Geografia', ' / ABDN', 'Matematyka'];
+var six =   ['', 'Fizyka', 'Matematyka', 'Historia', '', 'Historia'];
+var seven = ['', 'WF / Angielski', 'EDB', 'Polski', '', ' / Niemiecki'];
+var eight = ['', 'Matematyka', 'Niemiecki / Angielski', 'Polski', '', ''];
+var nine =  [''', Polski', ' / WF', 'Angielski / WF', '', ''];
+var ten =   ['', '', '', 'Religia', '', ''];
+
+
+
+let scheduledMessage0 = new cron.CronJob('05 10 06 * * *', () => {
+  var d = new Date();
+var msge = zero[d.getDay()];
+let channel = client.channels.cache.get('777899451914125342');
+        channel.send("@everyone Rozpoczyna się " + msge);
+  });
+
+
 
 let scheduledMessage1 = new cron.CronJob('05 00 07 * * *', () => {
   var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "<@&776793497978011651> 8:00, zaczynamy WF!";
-    wiadomosc[2] = "Dzisiaj lekcji brak :sob:";
-    wiadomosc[3] = "<@&776793497978011651> 8:00, zaczynamy WF!";
-    wiadomosc[4] = "Dzisiaj lekcji brak :sob:";
-    wiadomosc[5] = "Dzisiaj lekcji brak :sob:";
-var msge = wiadomosc[d.getDay()];
+var msge = one[d.getDay()];
 let channel = client.channels.cache.get('777899451914125342');
-        channel.send(msge);
+        channel.send("@everyone Rozpoczyna się " + msge);
   });
+
+
 
   let scheduledMessage2 = new cron.CronJob('05 50 07 * * *', () => {
     var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "@everyone 8:50, czas na matematykę!";
-    wiadomosc[2] = "Dzisiaj lekcji brak :sob:";
-    wiadomosc[3] = "@everyone 8:50, czas na geografię!";
-    wiadomosc[4] = "Dzisiaj lekcji brak :sob:";
-    wiadomosc[5] = "<@&771653446621200394> 8:50, zaczynamy ABDN!";
-var msge = wiadomosc[d.getDay()];
-let channel = client.channels.cache.get('777899451914125342');
-        channel.send(msge);
+    var msge = two[d.getDay()];
+    let channel = client.channels.cache.get('777899451914125342');
+            channel.send("@everyone Rozpoczyna się " + msge);
   });
+
+
 
   let scheduledMessage3 = new cron.CronJob('05 40 8 * * *', () => {
     var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "@everyone 9:40, czas na PPR!";
-    wiadomosc[2] = "@everyone 9:40, zaczynamy chemię!";
-    wiadomosc[3] = "@everyone 9:40, <@&771653446621200394> czas na WF! <@&776793497978011651>, wy macie anigelski!";
-    wiadomosc[4] = "Dzisiaj lekcji brak :sob:";
-    wiadomosc[5] = "@everyone 9:40, czas na PBD!";
-var msge = wiadomosc[d.getDay()];
-let channel = client.channels.cache.get('777899451914125342');
-        channel.send(msge);
+    var msge = three[d.getDay()];
+    let channel = client.channels.cache.get('777899451914125342');
+            channel.send("@everyone Rozpoczyna się " + msge);
   });
+
+
 
   let scheduledMessage4 = new cron.CronJob('05 30 9 * * *', () => {
     var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "@everyone 10:30, zaczynami informatykę stosowaną!";
-    wiadomosc[2] = "@everyone 10:30, <@&771653446621200394> zapraszam na informatykę! <@&776793497978011651>, zapraszam na angielski!";
-    wiadomosc[3] = "@everyone 10:30, przyszedł czas na zajęcia z wychowawcą!";
-    wiadomosc[4] = "@everyone 10:30, zaczynamy dzień historią!";
-    wiadomosc[5] = "@everyone 10:30, czas na religię!";
-var msge = wiadomosc[d.getDay()];
-let channel = client.channels.cache.get('777899451914125342');
-        channel.send(msge);
+    var msge = four[d.getDay()];
+    let channel = client.channels.cache.get('777899451914125342');
+            channel.send("@everyone Rozpoczyna się " + msge);
   });
+
+
 
   let scheduledMessage5 = new cron.CronJob('05 20 10 * * *', () => {
     var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "@everyone 11:20, druga godzina informatyki stosowanej!";
-    wiadomosc[2] = "@everyone 11:20, <@&771653446621200394> zapraszam na WF! <@&776793497978011651>, czeka was informatyka!";
-    wiadomosc[3] = "@everyone 11:20, czas na PBD!";
-    wiadomosc[4] = "@everyone 11:20, nadszedł czas na biologię!";
-    wiadomosc[5] = "@everyone 11:20, zaczynamy PPR!";
-var msge = wiadomosc[d.getDay()];
-let channel = client.channels.cache.get('777899451914125342');
-        channel.send(msge);
+    var msge = five[d.getDay()];
+    let channel = client.channels.cache.get('777899451914125342');
+            channel.send("@everyone Rozpoczyna się " + msge);
   });
+
+
 
   let scheduledMessage6 = new cron.CronJob('05 20 11 * * *', () => {
     var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "@everyone 12:20, zaczynamy fizykę!";
-    wiadomosc[2] = "@everyone 12:20, czas na matematykę!";
-    wiadomosc[3] = "@everyone 12:20, <@&771653446621200394> zapraszam na niemiecki! <@&776793497978011651> czas poćwiczyć na WF!";
-    wiadomosc[4] = "@everyone 12:20, czas na PSI!";
-    wiadomosc[5] = "@everyone 12:20, czas na PSI!";
-var msge = wiadomosc[d.getDay()];
+var msge = six[d.getDay()];
 let channel = client.channels.cache.get('777899451914125342');
-        channel.send(msge);
+        channel.send("@everyone Rozpoczyna się " + msge);
   });
+
+
 
   let scheduledMessage7 = new cron.CronJob('05 10 12 * * *', () => {
     var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "@everyone 13:10, zaczynamy edukację regionalną!";
-    wiadomosc[2] = "@everyone 13:10, czas na historię!";
-    wiadomosc[3] = "@everyone 13:10, religia się rozpoczyna!";
-    wiadomosc[4] = "@everyone 13:10, bądźmy bezpieczni na EDB!";
-    wiadomosc[5] = "@everyone 13:10, więc czas na RZEŹ... ( aka. filozofia :smile: ) ";
-var msge = wiadomosc[d.getDay()];
-let channel = client.channels.cache.get('777899451914125342');
-        channel.send(msge);
+    var msge = seven[d.getDay()];
+    let channel = client.channels.cache.get('777899451914125342');
+            channel.send("@everyone Rozpoczyna się " + msge);
   });
+
+
 
   let scheduledMessage8 = new cron.CronJob('05 00 13 * * *', () => {
     var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "@everyone 14:00, <@&771653446621200394> rozpoczyna angielski! <@&776793497978011651> sprechen Sie Deutsch?";
-    wiadomosc[2] = "@everyone 14:00, <@&771653446621200394> rozpoczyna angielski! <@&776793497978011651> sprechen Sie Deutsch?";
-    wiadomosc[3] = "@everyone 14:00, czas na polski!";
-    wiadomosc[4] = "@everyone 14:00, matematyka przyszłością!";
-    wiadomosc[5] = "<@&771653446621200394> 14:00, tworzymy stroni internetowe!";
-var msge = wiadomosc[d.getDay()];
+var msge = eight[d.getDay()];
 let channel = client.channels.cache.get('777899451914125342');
-        channel.send(msge);
+        channel.send("@everyone Rozpoczyna się " + msge);
   });
+
+
 
     let scheduledMessage9 = new cron.CronJob('05 50 13 * * *', () => {
       var d = new Date();
-        var wiadomosc = new Array(5);
-        wiadomosc[1] = "@everyone 14:50, zaczynamy polski!";
-        wiadomosc[2] = "<@&771653446621200394> 14:50, niemiecki się zaczyna!";
-        wiadomosc[3] = "@everyone 14:50, druga godzina polskiego!";
-        wiadomosc[4] = "@everyone 14:50, <@&771653446621200394> zaczyna WF! <@&776793497978011651> idzie tworzyć strony internetowe!";
-        wiadomosc[5] = "<@&771653446621200394> 14:50, TSI - ostatnia lekcja w tygodniu!";
-    var msge = wiadomosc[d.getDay()];
-    let channel = client.channels.cache.get('777899451914125342');
-            channel.send(msge);
+      var msge = nine[d.getDay()];
+      let channel = client.channels.cache.get('777899451914125342');
+              channel.send("@everyone Rozpoczyna się " + msge);
   });
 
-  let scheduledMessage10 = new cron.CronJob('05 40 14 * * *', () => {
+
+
+  let scheduledMessage10 = new cron.CronJob('05 51 14 * * *', () => {
     var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "Dzisiaj lekcji brak :sob:";
-    wiadomosc[2] = "Dzisiaj lekcji brak :sob:";
-    wiadomosc[3] = "<@&776793497978011651> 15:40, zaczynami TSI!";
-    wiadomosc[4] = "<@&776793497978011651> 15:40, czas na ABDN!";
-    wiadomosc[5] = "Dzisiaj lekcji brak :sob:";
-var msge = wiadomosc[d.getDay()];
-let channel = client.channels.cache.get('777899451914125342');
-        channel.send(msge);
-  });
-/*
-  client.on('message', msg => {
-    if (msg.content === 'test') {
-      var d = new Date();
-    var wiadomosc = new Array(5);
-    wiadomosc[1] = "Dzisiaj lekcji brak :sob:";
-    wiadomosc[2] = "Dzisiaj lekcji brak :sob:";
-    wiadomosc[3] = "<@&776793497978011651> 15:40, zaczynami TSI!";
-    wiadomosc[4] = "<@&776793497978011651> 15:40, czas na ABDN!";
-    wiadomosc[5] = "Dzisiaj lekcji brak :sob:";
-var msge = wiadomosc[d.getDay()];
-      msg.reply(msge);
-    }
+    var msge = ten[d.getDay()];
+    let channel = client.channels.cache.get('777899451914125342');
+            channel.send("@everyone Rozpoczyna się " + msge);
   });
 
-  client.on('message', (message) => {
-    if (message.content.startsWith (prefix + "ping")) {
-      message.reply("pong!");
-    }
-    if (message.content.startsWith (prefix + "zapisz")) {
-
-      const args = message.content.slice(8).trim().split(/ +/g);
-      let [nazwa, przedmiot, rok, miesiac, dzien, godzina, minuta, platforma] = args;
-
-      client.zadania [nazwa] = {
-        namee: nazwa,
-        przedmiott: przedmiot,
-        year: rok,
-        month: miesiac,
-        day: dzien,
-        hour: godzina,
-        minute: minuta,
-        platform: platforma
-      
-      }
-
-      fs.writeFile ("./zadania.json", JSON.stringify (client.zadania, null, 4), err => {
-if (err) throw err;
-message.channel.send ("zapisano");
-      });
-    }
-    if(message.content.startsWith (prefix + "wczytaj")) {
-      name = message.content.slice(9).trim();
-
-let przedmiot = client.zadania[name].przedmiott;
-let year = client.zadania[name].year;
-let month = client.zadania[name].month;
-let day = client.zadania[name].day;
-let hour = client.zadania[name].hour;
-let minute = client.zadania[name].minute;
-let platform = client.zadania[name].platform;
-message.channel.send ("Nazwa: " + name + "\nPrzedmiot: " + przedmiot + "\nData: " + day + "." + month + "." + year + "\nGodzina: " + hour + "." + minute + "\nPlatforma: " + platform );
-    };
-
-    if(message.content.startsWith (prefix + "czysc")) { 
-fs.rm("./zadania.json", err => {
-if (err) throw err;
-fs.copyFile("./def/zadania.json", "./", err =>{
-  if (err) throw err;
-});
-message.channel.send ("wyczyszczono");
-});
-
-    };
-  });
-*/
+  scheduledMessage0.start()
   scheduledMessage1.start()
   scheduledMessage2.start()
   scheduledMessage3.start()
@@ -216,5 +134,34 @@ message.channel.send ("wyczyszczono");
   scheduledMessage9.start()
   scheduledMessage10.start()
 
+
+  client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+  
+    if (command === 'ping') {
+      message.channel.send('Pong.');
+    } else if (command === 'beep') {
+      message.channel.send('Boop.');
+    } else if (command === 'dzien') {
+      
+      var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Niedziela";
+weekday[1] = "Poniedziałek";
+weekday[2] = "Wtorek";
+weekday[3] = "Środa";
+weekday[4] = "Czwartek";
+weekday[5] = "Piątek";
+weekday[6] = "Sobota";
+
+var n = weekday[d.getDay()];
+  
+      message.channel.send(n);
+    }
+    // other commands...
+  });
 client.login(process.env.BOT_TOKEN);
 //process.env.BOT_TOKEN
